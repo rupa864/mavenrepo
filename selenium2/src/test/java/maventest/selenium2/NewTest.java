6822e7e4f68428094dc7fe770bee489d0f802d56
RupaGowd <RupaGowd@DESKTOP-NLFF423>
2019-11-11 12:53:38 +0530

    comitted

---
+++ selenium2/src/test/java/maventest/selenium2/NewTest.java
@@ -0,0 +1,9 @@
+package maventest.selenium2;
+import org.testng.annotations.Test;
+public class NewTest {
+  @Test
+  public void test() 
+  {
+	  System.out.println("Executed Successfully");
+  }
+}
